a7bdd18a0ad40982ea700caaf9af6fcbb5683379
spelkey-ucd <spelkey@ucdavis.edu>
2025-12-08 09:18:53 -0500

    local dev default

--- devops/cmds/init-local-dev.sh
+++ devops/cmds/init-local-dev.sh
@@ -8,10 +8,15 @@ set -e
 CMDS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$CMDS_DIR/config.sh"
 
+VERSION=$1
+if [ -z "$VERSION" ]; then
+  VERSION="main"
+fi
+
 cd $CMDS_DIR
 
 echo "Building local-dev docker image..."
-./build-local-dev.sh
+./build-local-dev.sh $VERSION
 echo "Local-dev docker image built."
 
 if [ ! -f "$LOCAL_DEV_DIR/.env" ]; then
